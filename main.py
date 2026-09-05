4640013dc9fca38293401d90c06ba455c37fd60b
E0strix <collider196@gmail.com>
2026-09-05 13:10:49 +0500

    updated gitignore, skeleton of subject_entry()

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
         print("Choose an option")
         print("------------------------")
         print("\n1- Add subjects (max. 12)")
-        op = input("Choose from the options above:")
+        op = input("Choose from the options above: ")
 
         if op == "1":
             subject_entry()
@@ -21,7 +21,25 @@ def main():
 
 
 def subject_entry():
-    while True:
-        subjects = input("\nEnter no. subjects studying:")
+    while True:  # No. of subjects checker
+        try:
+            subject_count = int(input("\nEnter no. subjects studying: "))
+
+            if subject_count > 12:
+                print("No. of subjects should be 12 or less")
+            elif subject_count < 1:
+                print("You must enter at least 1 subject")
+            else:
+                break
+        except ValueError:
+            print("Incorrect! Please enter a number")   
+
+    for x in range (1, subject_count + 1):
+            subject = input(f"\nEnter the subject {x}: ")
+        
+        
+
+
+
 
 main()
